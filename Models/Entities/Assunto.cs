ddb804ce57f743cf986aadc6fe83943431952f9b
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 01:32:14 -0300

    Feat: Criado Entidade Assunto

---
+++ Models/Entities/Assunto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPI.Models.Entities
+{
+    [Table("Assunto")]
+    public class Assunto
+    {
+        [Key]
+        [Column("codAs")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "A descrição não pode exceder 20 caracteres.")]
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+
+        // Relacionamento N-N com Livro
+        public ICollection<LivroAssunto> Livros { get; set; }
+    }
+}
