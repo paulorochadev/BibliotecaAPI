6956be70cfef2e4214a033aaa189029694ab7835
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 01:32:41 -0300

    Feat: Criado Entidade LivroAutor

---
+++ Models/Entities/LivroAutor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaAPI.Models.Entities
+{
+    [Table("Livro_Autor")]
+    public class LivroAutor
+    {
+        [Column("Livro_Codi")]
+        public int LivroId { get; set; }
+
+        [Column("Autor_CodAu")]
+        public int AutorId { get; set; }
+
+        public Livro Livro { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
