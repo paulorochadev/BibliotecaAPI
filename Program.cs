89cdbe2e7f34c085e486d95349364a7027d205f8
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 02:54:46 -0300

    Chore: Ajustado arquivo Program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using BibliotecaAPI.Repositories;
+using BibliotecaAPI.Repositories.Implementations;
+using BibliotecaAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configuração do Banco de Dados
+builder.Services.AddDbContext<BibliotecaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaConnection")));
+
+// Configuração de repositórios e serviços
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IAutorRepository, AutorRepository>();
+builder.Services.AddScoped<IAssuntoRepository, AssuntoRepository>();
 
 var app = builder.Build();
 
