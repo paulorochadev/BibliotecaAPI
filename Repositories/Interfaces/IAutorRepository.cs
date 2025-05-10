5008f611e443271a7bf4b53c40174a5ef35e8386
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 02:47:36 -0300

    Feat: Criado IAutorRepository

---
+++ Repositories/Interfaces/IAutorRepository.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaAPI.Models.Entities;
+
+namespace BibliotecaAPI.Repositories.Interfaces
+{
+    public interface IAutorRepository
+    {
+        Task<IEnumerable<Autor>> GetAllAsync();
+        Task<Autor?> GetByIdAsync(int id);
+        Task<Autor> CreateAsync(Autor autor);
+        Task UpdateAsync(Autor autor);
+        Task DeleteAsync(int id);
+    }
+}
