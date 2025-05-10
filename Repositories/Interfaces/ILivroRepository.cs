963e33922fe6a3a0ae81375363823be7ccb666f7
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 02:25:50 -0300

    Feat: Criado ILivroRepository

---
+++ Repositories/Interfaces/ILivroRepository.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaAPI.Models.Entities;
+
+namespace BibliotecaAPI.Repositories.Interfaces
+{
+    public interface ILivroRepository
+    {
+        Task<IEnumerable<Livro>> GetAllAsync();
+        Task<Livro?> GetByIdAsync(int id);
+        Task<Livro> CreateAsync(Livro livro);
+        Task UpdateAsync(Livro livro);
+        Task DeleteAsync(int id);
+    }
+}
