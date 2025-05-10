29fb79b250c015b27c8d6ed9ba797fb38772b476
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 01:31:51 -0300

    Feat: Criado Entidade Autor

---
+++ Models/Entities/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPI.Models.Entities
+{
+    [Table("Autor")]
+    public class Autor
+    {
+        [Key]
+        [Column("CodAu")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40, ErrorMessage = "O nome não pode exceder 40 caracteres.")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        // Relacionamento N-N com Livro
+        public ICollection<LivroAutor> Livros { get; set; }
+    }
+}
