3b84cc0ec74f235448b44f4eba2ca1defa418ae2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 01:33:10 -0300

    Feat: Criado Entidade LivroAssunto

---
+++ Models/Entities/LivroAssunto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BibliotecaAPI.Models.Entities
+{
+    [Table("Livro_Assunto")]
+    public class LivroAssunto
+    {
+        [Column("Livro_Codi")]
+        public int LivroId { get; set; }
+
+        [Column("Assunto_codAs")]
+        public int AssuntoId { get; set; }
+
+        public Livro Livro { get; set; }
+        public Assunto Assunto { get; set; }
+    }
+}
