10d7c896a3b156f447fc77c6acf2bc185b551fe2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 02:48:12 -0300

    Feat: Criado IAssuntoRepository

---
+++ Repositories/Interfaces/IAssuntoRepository.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaAPI.Models.Entities;
+
+namespace BibliotecaAPI.Repositories.Interfaces
+{
+    public interface IAssuntoRepository
+    {
+        Task<IEnumerable<Assunto>> GetAllAsync();
+        Task<Assunto?> GetByIdAsync(int id);
+        Task<Assunto> CreateAsync(Assunto assunto);
+        Task UpdateAsync(Assunto assunto);
+        Task DeleteAsync(int id);
+    }
+}
