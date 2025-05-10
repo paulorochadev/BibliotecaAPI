f8b21104985330c34a6bfd16ec8abb076fa95ec2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-10 01:07:02 -0300

    Criado Projeto Backend API

--- obj/Debug/net8.0/BibliotecaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BibliotecaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac484ec86d4530baca36b5858dabf2c903edc1b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
