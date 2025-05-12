9916d8eff7caefee8476e91cea77ce25fa395b5a
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 10:33:27 -0300

    Feat: Criado PrecoLivroDTO

---
+++ Models/DTOs/PrecoLivroDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaAPI.Models.DTOs
+{
+    public class PrecoLivroDTO
+    {
+        public int Id { get; set; }
+        public int LivroId { get; set; }
+        public decimal Valor { get; set; }
+        public string TipoCompra { get; set; }
+    }
+}
