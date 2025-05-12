d8352611705e0f1249280d2e8d556acb0c6aaaf4
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 10:52:50 -0300

    Feat: Criado IAssuntoService

---
+++ Services/Interfaces/IAssuntoService.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaAPI.Models.DTOs;
+
+namespace BibliotecaAPI.Services.Interfaces
+{
+    public interface IAssuntoService
+    {
+        Task<IEnumerable<AssuntoDTO>> GetAllAssuntosAsync();
+        Task<AssuntoDTO?> GetAssuntoByIdAsync(int id);
+        Task<AssuntoDTO> CreateAssuntoAsync(AssuntoDTO assuntoDto);
+        Task UpdateAssuntoAsync(int id, AssuntoDTO assuntoDto);
+        Task DeleteAssuntoAsync(int id);
+    }
+}
