0672f6f6012d3ca86ee97ad9f5e280df0e54aa98
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 11:52:00 -0300

    Feat: Criado RelatorioLivrosPorAutorDTO

---
+++ Models/DTOs/RelatorioLivrosPorAutorDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace BibliotecaAPI.Models.DTOs
+{
+    public class RelatorioLivrosPorAutorDTO
+    {
+        public int AutorId { get; set; }
+        public string AutorNome { get; set; }
+        public int LivroId { get; set; }
+        public string LivroTitulo { get; set; }
+        public string Editora { get; set; }
+        public int Edicao { get; set; }
+        public string AnoPublicacao { get; set; }
+        public string Assuntos { get; set; }
+        public string Precos { get; set; }
+        public int TotalLivros { get; set; }
+    }
+}
