28f5510e6b7a6a24096c140ae515c3ecfb0b6e88
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 10:51:55 -0300

    Feat: Criado ILivroService

---
+++ Services/Interfaces/ILivroService.cs
@@ -0,0 +1,15 @@
+﻿using BibliotecaAPI.Models.DTOs;
+
+namespace BibliotecaAPI.Services.Interfaces
+{
+    public interface ILivroService
+    {
+        Task<IEnumerable<LivroDTO>> GetAllLivrosAsync();
+        Task<LivroDTO?> GetLivroByIdAsync(int id);
+        Task<LivroDTO> CreateLivroAsync(LivroDTO livroDto);
+        Task UpdateLivroAsync(int id, LivroDTO livroDto);
+        Task DeleteLivroAsync(int id);
+        Task<IEnumerable<PrecoLivroDTO>> GetPrecosByLivroIdAsync(int livroId);
+        Task<PrecoLivroDTO> AddPrecoToLivroAsync(int livroId, PrecoLivroDTO precoDto);
+    }
+}
