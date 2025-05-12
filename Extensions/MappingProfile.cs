875f5e44f0a4ccd151b99f89376dfe9ed1fdac41
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 11:05:11 -0300

    Feat: Criado MappingProfile

---
+++ Extensions/MappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BibliotecaAPI.Models.DTOs;
+using BibliotecaAPI.Models.Entities;
+
+namespace BibliotecaAPI.Extensions
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Mapeamento para Livro
+            CreateMap<Livro, LivroDTO>()
+                .ReverseMap();
+
+            // Mapeamento para Autor
+            CreateMap<Autor, AutorDTO>()
+                .ReverseMap();
+
+            // Mapeamento para Assunto
+            CreateMap<Assunto, AssuntoDTO>()
+                .ReverseMap();
+
+            // Mapeamento para PrecoLivro
+            CreateMap<PrecoLivro, PrecoLivroDTO>()
+                .ReverseMap();
+        }
+    }
+}
