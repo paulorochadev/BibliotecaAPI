2442c704ba2310aee4a3a98cf5ed980d29ff3d92
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 10:52:20 -0300

    Feat: Criado IAutorService

---
+++ Services/Interfaces/IAutorService.cs
@@ -0,0 +1,13 @@
+﻿using BibliotecaAPI.Models.DTOs;
+
+namespace BibliotecaAPI.Services.Interfaces
+{
+    public interface IAutorService
+    {
+        Task<IEnumerable<AutorDTO>> GetAllAutoresAsync();
+        Task<AutorDTO?> GetAutorByIdAsync(int id);
+        Task<AutorDTO> CreateAutorAsync(AutorDTO autorDto);
+        Task UpdateAutorAsync(int id, AutorDTO autorDto);
+        Task DeleteAutorAsync(int id);
+    }
+}
