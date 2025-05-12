f743e3bd6551713668bf5772aa9ccb8c2b5a8649
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 19:51:18 -0300

    Fix: Ajustado dependências Repositories e Services

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using BibliotecaAPI.Repositories.Implementations;
 using BibliotecaAPI.Repositories.Interfaces;
 using BibliotecaAPI.Services.Implementations;
 using BibliotecaAPI.Services.Interfaces;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,17 +19,33 @@ builder.Services.AddSwaggerGen();
 // Configuração do AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
 
-// Configuração do Banco de Dados
-builder.Services.AddDbContext<BibliotecaContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaConnection")));
+//// Configuração do Banco de Dados
+//builder.Services.AddDbContext<BibliotecaContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaConnection"));
+//    options.EnableDetailedErrors(); // mensagens de erro mais detalhadas
+//    options.EnableSensitiveDataLogging(); // ver valores reais nos logs
+//    options.LogTo(Console.WriteLine, LogLevel.Information); // Log das consultas SQL
+//});
 
-// Configuração do Repositories
-builder.Services.AddScoped<ILivroRepository, LivroRepository>();
-builder.Services.AddScoped<IAutorRepository, AutorRepository>();
-builder.Services.AddScoped<IAssuntoRepository, AssuntoRepository>();
+// Configuração conexão SQL
+builder.Services.AddScoped<IDbConnection>(provider =>
+    new SqlConnection(builder.Configuration.GetConnectionString("BibliotecaConnection")));
+
+//// Configuração do Repositories (EF Core)
+//builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+//builder.Services.AddScoped<IAutorRepository, AutorRepository>();
+//builder.Services.AddScoped<IAssuntoRepository, AssuntoRepository>();
+
+// Configuração do Repositories (ADO.NET/Dapper)
+builder.Services.AddScoped<ILivroRepository, LivroAdoRepository>();
+builder.Services.AddScoped<IAutorRepository, AutorAdoRepository>();
+builder.Services.AddScoped<IAssuntoRepository, AssuntoAdoRepository>();
 
 // Configuração do Services
 builder.Services.AddScoped<ILivroService, LivroService>();
+builder.Services.AddScoped<IAutorService, AutorService>();
+builder.Services.AddScoped<IAssuntoService, AssuntoService>();
 
 // Configuração do CORS
 builder.Services.AddCors(options =>
