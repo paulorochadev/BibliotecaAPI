0376ad34ca5047105f0a118c070aeaf19fffb9e2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 10:32:57 -0300

    Feat: Criado Entidade PrecoLivro

---
+++ Models/Entities/PrecoLivro.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BibliotecaAPI.Models.Entities
+{
+    public class PrecoLivro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Livro_Codi")]
+        public int LivroId { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Valor { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string TipoCompra { get; set; } // "balcao", "self-service", "internet", "evento"
+
+        public Livro Livro { get; set; }
+    }
+}
